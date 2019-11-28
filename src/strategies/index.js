5694a430b911583022626445ae8b289c3f7b2a1d
taras.panasiuk <taras.panasiuk@ezlo.com>
2019-11-28 21:59:34 +0200

    add DynamicRoutingStrategy

--- src/strategies/index.js
+++ src/strategies/index.js
@@ -1,7 +1,9 @@
 import AbstractStrategy from './abstract'
 import JWTAuthStrategy from './jwt'
+import DynamicRoutingStrategy from './dynamic-routing'
 
 export {
   AbstractStrategy,
   JWTAuthStrategy,
+  DynamicRoutingStrategy,
 }
